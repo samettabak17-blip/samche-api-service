d369fdc4d5e5ae6b967eede0676f8ef21ea7342c
samettabak17-blip <samettabak17@gmail.com>
2026-09-14 03:48:42 +0400

    chore: clean temporary test artifacts

--- scripts/launch_detached.js
+++
@@ -1,9 +0,0 @@
-import { spawn } from 'node:child_process';
-
-const child = spawn(process.execPath, ['scripts/verify_real_external_acceptance.js'], {
-  cwd: 'C:/Users/smttb/Documents/samche-api-service',
-  detached: true,
-  stdio: 'ignore'
-});
-child.unref();
-console.log('Detached child PID:', child.pid);
